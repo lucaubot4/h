11f676db2d9d96cb763d18ccee933c9e9626d698
lucaubot4 <69604257+lucaubot4@users.noreply.github.com>
2020-11-18 09:58:00 +0100

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,44 +18,27 @@ from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
-PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+PM_START_TEXT = ""PM_START_TEXT  =  "" "
+Ciao, mi chiamo {}! Se hai domande su come usarmi, leggi /help.
+Sono un bot manager di gruppo costruito in python3.
+"" "
 
-I'm a group manager bot built in python3, using the python-telegram-bot library, and am fully opensource; \
-you can find what makes me tick [here](github.com/PaulSonOfLars/tgbot)!
-
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
-
-You can find the list of available commands with /help.
-
-If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
-"""
-
-HELP_STRINGS = """
-Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
-the things I can help you with.
-
-*Main* commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
-
+HELP_STRINGS  =  "" "
+Ehilà! Il mio nome è *{}*.
+Sono un bot di gestione di gruppi modulare con alcuni divertenti extra! Dai un'occhiata a quanto segue per avere un'idea di alcuni di \
+le cose in cui posso aiutarti.
+* Principali * comandi disponibili:
+ - / start: avvia il bot
+ - / help: PM sei tu questo messaggio.
+ - / help <nome modulo>: le informazioni del PM su quel modulo.
+ - / donate: informazioni su come donare!
+ - /impostazioni:
+   - in PM: ti invierà le impostazioni per tutti i moduli supportati.
+   - in un gruppo: ti reindirizzerà a pm, con tutte le impostazioni della chat.
 {}
-And the following:
-""".format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
+E il seguente:
+"" " . format ( dispatcher . bot . first_name , " "  if  not  ALLOW_EXCL  else  " \ n Tutti i comandi possono essere usati con / o!. \ n " )
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for my creator to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
